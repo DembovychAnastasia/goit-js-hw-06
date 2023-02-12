d910133dada3809af851877e7a411a3d07a3c3f6
DembovychAnastasia <anastasiya2591@gmail.com>
2023-02-12 16:03:09 +0200

    update task 2, task - 6, task -7

--- js/task-02.js
+++ js/task-02.js
@@ -9,13 +9,16 @@ const ingredients = [
 
 // знаходимо список 
 const container = document.querySelector("#ingredients"); 
+const arrayItem = [];
 // створюємо елемент списку
 ingredients.forEach((ingredientItem) => {
-  const liEl = document.createElement('li');
-  liEl.textContent = ingredientItem;
-  liEl.classList.add('item');
-  container.append(liEl);
-});
+  const liEl = document.createElement('li')
+  liEl.textContent = ingredientItem
+  liEl.classList.add('item')
+  arrayItem.push(liEl)
+ 
+})
+ container.append(...arrayItem)
 
 
 
